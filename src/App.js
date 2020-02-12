2b372243914b179d9d5afb94ca30d08ee2c065e0
FrankZou21 <frankzou2000@hotmail.com>
2020-02-11 17:59:20 -0800

    created form and set value to state, made function to compare anagrams

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import config from './config.js'
+import Firebase from "firebase";
 
 function App() {
+
+  //Nothing at data[0] array starts from data[1]
+  const [data, setData] = useState("")
+  const [input, setInput] = useState("")
+
+  useEffect(() => {
+    Firebase.initializeApp(config);
+    let ref = Firebase.database().ref("/");
+    ref.on("value", snapshot => {
+      const state = snapshot.val();
+      setData(state);
+    });
+  }, [])
+
+  function compare (inputStr) {
+    const sortedStr = inputStr.split("").sort().join("");
+    let count = 0;
+    for (let i = 1; i < data.length; i++) {
+      const sortedData = data[i].split("").sort().join("");
+      if (sortedStr === sortedData) {
+        count++;
+      }
+    }
+    return count;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <p>Find the Anagrams!</p>
+    <form>
+      <input onChange={inp => {setInput(inp.target.value)}} value={input} />
+    </form>
+    <p>{compare(input)} anagrams found.</p>
+    
     </div>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,12 @@
+const config = {
+  apiKey: "AIzaSyCiGYMT0mOAU-qW6z6926Mczh_FbaEpah8",
+  authDomain: "parkchamp-88e0b.firebaseapp.com",
+  databaseURL: "https://parkchamp-88e0b.firebaseio.com",
+  projectId: "parkchamp-88e0b",
+  storageBucket: "parkchamp-88e0b.appspot.com",
+  messagingSenderId: "247275727555",
+  appId: "1:247275727555:web:fa9cb922e2efa59a391076",
+  measurementId: "G-4L0ZZLT7VD"
+};
+
+export default config;
